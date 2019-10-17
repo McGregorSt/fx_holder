ea72c6465f63417e9f866f8b38c1a8e0093eaa9c
McGregorSt <stasiak.gregor@gmail.com>
2019-10-17 19:01:07 +0200

    Updates

---
+++ precache-manifest.44df9666f5876a85c684877059ccff18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a24b56f747fccfcac2061502bdfbcea2",
+    "url": "/fx_holder/index.html"
+  },
+  {
+    "revision": "033628a1f208985d02d1",
+    "url": "/fx_holder/static/css/main.95c3feed.chunk.css"
+  },
+  {
+    "revision": "dceb0d34a4c2588ab816",
+    "url": "/fx_holder/static/js/2.3a2d92f6.chunk.js"
+  },
+  {
+    "revision": "033628a1f208985d02d1",
+    "url": "/fx_holder/static/js/main.9307bf4e.chunk.js"
+  },
+  {
+    "revision": "ec75dda99c7708825d2d",
+    "url": "/fx_holder/static/js/runtime-main.60f79fac.js"
+  }
+]);
